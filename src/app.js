6b4599a662463437e65174acf058c726b33ca57c
Cristiancsv <csv71@yahoo.com>
2022-07-06 17:10:53 -0300

    probando

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ import book from './routes/book-router.js'
 
 const app = express();
 
+//permite que el servidor entienda cosas
+app.use(express.json());
+
+ 
+
 app.use(book)
-
-
 
 export default app;
--- src/controllers/book-controller.js
+++ src/controllers/book-controller.js
@@ -1,4 +1,9 @@
+import {Book} from '../models/Book.js'
 
+
+//función principal que carga base de datos
+const getApi = async () => {
+}
 
 export const getBooks = async (req, res) => {
 	res.send('acá no tenemos merca... perdón; no tenemos audiolibros')
