caf58d77007305bf71e097efde3814454d5526b0
Cristiancsv <csv71@yahoo.com>
2022-07-07 00:56:26 -0300

    Logré cargar datos a la tabla 'books' pero sin relación con la de genders

--- src/controllers/book-controller.js
+++ src/controllers/book-controller.js
@@ -1,10 +1,43 @@
 import {Book} from '../models/Book.js'
 
 
-//función principal que carga base de datos
+ const array  = [
+ {id:43,
+ nombre:"El viejo arbol",
+ img:"https://www.laprensa.com.ar/multimedios/imgs/137943_620.jpg",
+ link:"https://www.youtube.com/watch?v=deQaA80k2_8",
+ des:"descripción del arbol viejo"
+},
+{id:23,
+nombre:"EL ERIZO Y EL GLOBO",
+img:"https://storage.googleapis.com/www-paredro-com/uploads/2018/10/Leches-La-Laguna-el-significado-del-logo-de-LALA1.jpg",
+link:"https://www.youtube.com/watch?v=9SiNFya55Fo",
+des:"esta descripción del 2"}]
+
+
+
+// INTENTO DE función principal que carga base de datos 
 const getApi = async () => {
+  const db = await Book.findAll()
+  
+  if(db.length === 0){
+  array.map(async (e) => {
+            await Book.findOrCreate({
+            	where : {
+            		name : e.nombre,
+            		img: e.img,
+            		description: e.des,
+            		link: e.link
+            	}
+            })})
+  return array} else {
+  	return db
+  }
+
+
 }
 
 export const getBooks = async (req, res) => {
-	res.send('acá no tenemos merca... perdón; no tenemos audiolibros');
+	const la = await getApi();
+	res.send(la);
 }
--- src/db/db.js
+++ src/db/db.js
@@ -8,7 +8,7 @@ import Sequelize from "sequelize";
 export const sequelize = new Sequelize(
 	/*name_database*/'facilito',
 	/*usuario*/'postgres',
-	/*contraseña*/'2325',{
+	/*contraseña*/'1234',{
 	host: 'localhost',/*donde está la database*/
     dialect: 'postgres'/*que database es*/
 });
