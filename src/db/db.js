caab5f3fa7135ac9b64520ffcd5e58ef55788b9b
Cristiancsv <csv71@yahoo.com>
2022-07-08 00:15:43 -0300

    config dotenv .env

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,11 @@
 import Sequelize from "sequelize";
-
+import 'dotenv/config'
+const {USER, PASSWORD, DB_NAME} = process.env;
 
 export const sequelize = new Sequelize(
-	/*name_database*/'facilito',
-	/*usuario*/'postgres',
-	/*contraseña*/'1234',{
+	/*name_database*/`${DB_NAME}`,
+	/*usuario*/`${USER}`,
+	/*contraseña*/`${PASSWORD}`,{
 	host: 'localhost',/*donde está la database*/
     dialect: 'postgres'/*que database es*/
 });
